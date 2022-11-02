b01d06bb00f9d3b638bdaab1a4a4d0d756435266
mandarha <111448138+mandarha@users.noreply.github.com>
2022-11-02 13:19:14 +0530

    Update first_streamlit.py

--- first_streamlit.py
+++ first_streamlit.py
@@ -1,10 +1,3 @@
-conn = snowflake.connector.connect(user = 'mandarha',
-                                    password = 'Snowflake1',
-                                    account = 'ya26020.central-india.azure',
-                                    warehouse = 'compute_wh',
-                                    database = 'sf_demo',
-                                    schema = 'sf_demo')
-
 connect_sf = pd.read_sql("use role accountadmin;",conn)
 
 salary_dis = pd.read_sql("select distinct salary from SF_DEMO.SF_DEMO.EMP_SALARY;",conn)
